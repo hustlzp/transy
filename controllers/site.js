aca99a321a7151f07d7f0322272514c058c0cd4b
hustlzp <hustlzp@qq.com>
2014-01-28 22:02:29 +0800

    when not signin, redirect to the signin page

--- controllers/site.js
+++ controllers/site.js
@@ -5,9 +5,14 @@
 var Article = require('../models/article');
 
 exports.index = function (req, res) {
-    Article.getByUser(req.cookies.user.id, function (err, articles) {
-        res.render('site/index', { articles: articles });
-    });
+    if (req.cookies.user) {
+        Article.getByUser(req.cookies.user.id, function (err, articles) {
+            res.render('site/index', { articles: articles });
+        });
+    } else {
+        res.render('account/signin');
+    }
+
 };
 
 exports.about = function (req, res) {
