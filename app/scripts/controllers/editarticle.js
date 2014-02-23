cbb4bad474919ca3ec4f4decf722765ddefccedf
hustlzp <hustlzp@qq.com>
2014-02-23 15:47:15 +0800

    now can remove para item in article

--- app/scripts/controllers/editarticle.js
+++ app/scripts/controllers/editarticle.js
@@ -1,11 +1,16 @@
 'use strict';
 
 angular.module('transyApp')
-  .controller('EditArticleCtrl', function ($scope, $rootScope, $location, article) {
+  .controller('EditArticleCtrl', function ($scope, $rootScope, $location, article, contextmenuService) {
     $rootScope.pageId = 'page-edit-article';
     $rootScope.pageTitle = '编辑文章';
+
     $scope.article = article;
     $scope.save = function () {
       $scope.article.$save();
+    };
+    $scope.remove = function(){
+      var index = contextmenuService.getIndex();
+      $scope.article.paraList.splice(index, 1);
     };
   });
--- app/scripts/directives/contextmenu.js
+++ app/scripts/directives/contextmenu.js
@@ -1,11 +1,23 @@
 'use strict';
 
 angular.module('transyApp')
-  .directive('contextmenu', function ($window) {
+  .service('contextmenuService', function () {
+    var index = 0;  // 私有变量，列表item索引
+
+    this.setIndex = function (value) {
+      index = value;
+    };
+
+    this.getIndex = function () {
+      return index;
+    };
+  })
+  .directive('contextmenu', function ($window, contextmenuService) {
     return {
       restrict: 'A',
       link: function (scope, element, attrs) {
         var menu = angular.element('.context-menu');
+
         var open = function (event, element) {
           element.css('top', event.clientY + 'px');
           element.css('left', event.clientX + 'px');
@@ -17,10 +29,14 @@ angular.module('transyApp')
         };
 
         // 在element上点击右键时，显示菜单
+        // 并将当前点击的index存储到service中
         element.bind('contextmenu', function (event) {
+          var index = element.data('index');
+
           scope.$apply(function () {
             event.preventDefault(); // 阻止浏览器默认的右键行为
             open(event, menu);
+            contextmenuService.setIndex(index);
           });
         });
 
