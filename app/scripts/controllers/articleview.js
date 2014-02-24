2adaff532a76f7cdc19d3f5cd456a8a9ec11a29c
hustlzp <hustlzp@qq.com>
2014-02-24 19:02:15 +0800

    use ng-repeat-start to repead HTML Element without wapper

--- app/scripts/controllers/articleview.js
+++ app/scripts/controllers/articleview.js
@@ -2,32 +2,9 @@
 
 angular.module('transyApp')
   .controller('ArticleViewCtrl', function ($scope, $rootScope, $location, article) {
-    var html = '';
-
-    // Build html for article content
-    angular.forEach(article.paraList, function (para) {
-      switch (para.type) {
-        case 'header':
-          html += '<h3 class="en">' + para.en + '</h3>';
-          html += '<h3 class="cn">' + para.cn + '</h3>';
-          break;
-        case 'text':
-          html += '<p class="en">' + para.en + '</p>';
-          html += '<p class="cn">' + para.cn + '</p>';
-          break;
-        case 'quote':
-          html += '<blockquote class="en">' + para.en + '</blockquote>';
-          html += '<blockquote class="cn">' + para.cn + '</blockquote>';
-          break;
-        case 'image':
-          html += '<p><img src="' + para.en + '">' + '"/></p>';
-          break;
-      }
-    });
     $rootScope.pageId = 'page-article';
     $rootScope.pageTitle = article.cnTitle;
     $scope.article = article;
-    $scope.html = html;
     $scope.edit = function () {
       $location.path('/article/' + article._id + '/edit');
     };
--- app/scripts/directives/adjustheight.js
+++ app/scripts/directives/adjustheight.js
@@ -7,15 +7,17 @@ angular.module('transyApp')
       scope: { val: '=watchTarget' },
       link: function (scope, element, attrs) {
         scope.$watch('val', function () {
+          // 这里需要使用timeout，这样的话就可以将function重新设置到event queue的最末端执行
+          // 这样就可以保证执行的时候HTML已经渲染完毕
           $timeout(function () {
             // 获取en、cn中的较大高度
-            var enHeight = element.find('.en').first().height();
-            var cnHeight = element.find('.cn').first().height();
+            var enHeight = element.find('.en').first().innerHeight();
+            var cnHeight = element.find('.cn').first().innerHeight();
             var height = (enHeight > cnHeight) ? enHeight : cnHeight;
 
             // 设置ec-divider的高度
             element.find('.ec-divider').height(height + 15);
-          });
+          }, 0);
         }, true);
       }
     };
