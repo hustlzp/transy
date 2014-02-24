ad05a121250b52eeeb88420dfd56728e13000140
hustlzp <hustlzp@qq.com>
2014-02-24 19:29:09 +0800

    now can switch show mode between ec, en and cn

--- app/scripts/controllers/articleview.js
+++ app/scripts/controllers/articleview.js
@@ -4,8 +4,27 @@ angular.module('transyApp')
   .controller('ArticleViewCtrl', function ($scope, $rootScope, $location, article) {
     $rootScope.pageId = 'page-article';
     $rootScope.pageTitle = article.cnTitle;
+
     $scope.article = article;
+
+    $scope.showEn = true;
+    $scope.showCn = true;
+
     $scope.edit = function () {
       $location.path('/article/' + article._id + '/edit');
     };
+
+    // 切换显示模式
+    $scope.showMode = function (mode) {
+      if (mode === 'ec') {
+        $scope.showEn = true;
+        $scope.showCn = true;
+      } else if (mode === 'en') {
+        $scope.showEn = true;
+        $scope.showCn = false;
+      } else if (mode === 'cn') {
+        $scope.showEn = false;
+        $scope.showCn = true;
+      }
+    }
   });
